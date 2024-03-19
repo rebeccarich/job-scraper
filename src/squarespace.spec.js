84808ae380d5b47a584c9e20bfed36c4bd608975
Rebecca Richards <rrichards@earnestresearch.com>
2024-03-19 23:13:44 +0000

    use gist as response data storage

--- src/squarespace.spec.js
+++ src/squarespace.spec.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 const { test, expect } = require('@playwright/test')
 import { CourierClient } from '@trycourier/courier'
-import fs from 'fs'
-import path from 'path'
-
-import squarespaceResults from './data/squarespace.json'
+import { Octokit } from '@octokit/core'
 
 const courier = new CourierClient({
   authorizationToken: process.env.COURIER_AUTH_TOKEN
 })
 
+const octokit = new Octokit({
+  auth: process.env.GH_AUTH_TOKEN
+})
+
 test('Squarespace scraper', async ({ page }) => {
+  const GIST_ID = '939866703c9699afe2c7806158345912'
+  const GIST_NAME = 'squarespace.json'
+
   await page.goto('https://www.squarespace.com/careers/engineering?location=dublin')
   const jobs = await page.$$eval(
     '#careers-engineering .careers-list--jobs .careers-list__items a',
@@ -28,7 +32,23 @@ test('Squarespace scraper', async ({ page }) => {
     }
   )
 
-  if (JSON.stringify(jobs) !== JSON.stringify(squarespaceResults)) {
+  let gist
+
+  try {
+    gist = await octokit.request(`GET /gists/${GIST_ID}`, {
+      gist_id: GIST_ID,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+  } catch (e) {
+    throw new Error(e)
+  }
+
+  const results = gist.data.files[GIST_NAME].content
+  console.log('Comparing against previous results', results)
+
+  if (JSON.stringify(jobs) !== JSON.stringify(results)) {
     const { requestId } = await courier.send({
       message: {
         to: {
@@ -40,8 +60,22 @@ test('Squarespace scraper', async ({ page }) => {
     })
     console.log('New Squarespace jobs found 🚀 Courier notification requested:', requestId)
     console.log('⚙️ Updating result set for next run...')
-    const resultsPath = path.join(__dirname, 'data', 'squarespace.json')
-    fs.writeFileSync(resultsPath, JSON.stringify(jobs))
+    try {
+      await octokit.request(`PATCH /gists/${GIST_ID}`, {
+        gist_id: GIST_ID,
+        files: {
+          [GIST_NAME]: {
+            content: JSON.stringify(jobs)
+          }
+        },
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+      console.log('Updated!')
+    } catch (e) {
+      throw new Error(e)
+    }
   } else {
     console.log('No new Squarespace jobs found 😞')
   }
