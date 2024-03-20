30a5a0c2cab803cc5d100b866cfb0ca1a83879c8
Rebecca Richards <rrichards@earnestresearch.com>
2024-03-20 00:11:01 +0000

    cleanup

--- src/squarespace.spec.js
+++ src/squarespace.spec.js
@@ -11,74 +11,101 @@ const octokit = new Octokit({
   auth: process.env.GH_AUTH_TOKEN
 })
 
+const COURIER_TEMPLATE_ID = '6XA45NXXD1MHJRGMKC6J3J5XNE63'
+const EMAIL_TO = 'rebeccarich1@gmail.com'
+
 test('Squarespace scraper', async ({ page }) => {
+  const COMPANY_NAME = 'Squarespace'
   const GIST_ID = '939866703c9699afe2c7806158345912'
   const GIST_NAME = 'squarespace.json'
+  const URL_TO_SCRAPE = 'https://www.squarespace.com/careers/engineering?location=dublin'
+  const SELECTOR = '#careers-engineering .careers-list--jobs .careers-list__items a'
 
-  await page.goto('https://www.squarespace.com/careers/engineering?location=dublin')
-  const jobs = await page.$$eval(
-    '#careers-engineering .careers-list--jobs .careers-list__items a',
-    (jobs) => {
-      const data = []
-      jobs.forEach((j) => {
-        const title = j.querySelector('.careers-list__item__title')?.innerHTML
-        const location = j.querySelector('.careers-list__item__locations')?.innerHTML
-        const href = j.href
-        if (location === 'Dublin, IE') {
-          data.push({ title, location, href })
-        }
-      })
-      return data
-    }
-  )
+  await page.goto(URL_TO_SCRAPE)
+
+  const jobs = await page.$$eval(SELECTOR, (jobs) => {
+    const data = []
+    jobs.forEach((j) => {
+      const title = j.querySelector('.careers-list__item__title')?.innerHTML
+      const location = j.querySelector('.careers-list__item__locations')?.innerHTML
+      const href = j.href
+      if (location === 'Dublin, IE') {
+        data.push({ title, location, href })
+      }
+    })
+    return data
+  })
 
   let gist
 
   try {
-    gist = await octokit.request(`GET /gists/${GIST_ID}`, {
-      gist_id: GIST_ID,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
-
-    const results = gist.data.files[GIST_NAME].content
-
-    if (JSON.stringify(jobs) !== results) {
-      const { requestId } = await courier.send({
-        message: {
-          to: {
-            email: 'rebeccarich1@gmail.com'
-          },
-          template: '6XA45NXXD1MHJRGMKC6J3J5XNE63',
-          data: { jobs, company: 'Squarespace' }
-        }
-      })
-      console.log('New Squarespace jobs found 🚀 Courier notification requested:', requestId)
-      console.log('⚙️ Updating result set for next run...')
-
-      try {
-        await octokit.request(`PATCH /gists/${GIST_ID}`, {
-          gist_id: GIST_ID,
-          files: {
-            [GIST_NAME]: {
-              content: JSON.stringify(jobs)
-            }
-          },
-          headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-          }
-        })
-        console.log('Updated!')
-      } catch (e) {
-        throw new Error(e)
-      }
-    } else {
-      console.log('No new Squarespace jobs found 😞')
-    }
+    console.log('Reading previous jobs data...')
+    gist = await readGist(GIST_ID)
   } catch (e) {
-    throw new Error(e)
+    console.error(e)
+    process.exit()
+  }
+
+  if (JSON.stringify(jobs) !== gist.data.files[GIST_NAME].content) {
+    try {
+      const { requestId } = await sendNotification(
+        EMAIL_TO,
+        COURIER_TEMPLATE_ID,
+        COMPANY_NAME,
+        jobs
+      )
+      console.log(`New ${COMPANY_NAME} jobs found 🚀 Courier notification requested:`, requestId)
+    } catch (e) {
+      console.error('Error sending notification', e)
+      process.exit()
+    }
+
+    try {
+      console.log('⚙️ Updating result set for next run...')
+      await updateGist(GIST_ID, GIST_NAME, jobs)
+      console.log('Updated!')
+    } catch (e) {
+      console.error(e)
+      process.exit()
+    }
+  } else {
+    console.log(`No new ${COMPANY_NAME} jobs found 😞`)
   }
 
   await expect(page).toHaveTitle(/Engineering Careers – Squarespace/)
 })
+
+async function readGist(id) {
+  return await octokit.request(`GET /gists/${id}`, {
+    gist_id: id,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+async function updateGist(id, fileName, newData) {
+  return await octokit.request(`PATCH /gists/${id}`, {
+    gist_id: id,
+    files: {
+      [fileName]: {
+        content: JSON.stringify(newData)
+      }
+    },
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+async function sendNotification(emailAddress, templateId, companyName, jobs) {
+  return await courier.send({
+    message: {
+      to: {
+        email: emailAddress
+      },
+      template: templateId,
+      data: { jobs, company: companyName }
+    }
+  })
+}
